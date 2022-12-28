e04d428b7399d08b59792dcda51629600fa942b3
ashwinikiran27 <121540745+ashwinikiran27@users.noreply.github.com>
2022-12-28 17:03:18 +0530

    Update A.java

--- GitHubProject/src/Com/cjc/A.java
+++ GitHubProject/src/Com/cjc/A.java
@@ -8,6 +8,9 @@ public class A {
 	public void m1() {
 		System.out.println("hellom m1");
 	}
+	public void m2() {
+		System.out.println("hellom m2");
+	}
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome in github");
