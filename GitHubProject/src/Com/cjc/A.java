dd9f86b6f348aac23d177ffd8d3ae6e5fc00e741
Dell <Dell@DESKTOP-Q02KMJK>
2022-12-28 16:58:41 +0530

    by xyz

--- GitHubProject/src/Com/cjc/A.java
+++ GitHubProject/src/Com/cjc/A.java
@@ -2,6 +2,12 @@ package Com.cjc;
 
 public class A {
 
+	
+	
+	
+	public void m1() {
+		System.out.println("hellom m1");
+	}
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome in github");
